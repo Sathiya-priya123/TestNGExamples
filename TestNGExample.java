997257d99aa89701f752bb9eeacbaa6549f2c521
sathiyapriya.s <sathiyapriya.s@qualitestgroup.com>
2023-01-10 14:30:46 +0530

    Examples for TestNG

---
+++ TestNGExample.java
@@ -0,0 +1,28 @@
+package TestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+@Test
+
+public class TestNGExample {
+
+    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String driverPath;
+    public WebDriver driver ;
+
+    @Test
+    public void verifyHomepageTitle() {
+
+        System.out.println("launching firefox browser");
+        //System.setProperty("webdriver.gecko.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = driver.getTitle();
+        //  Assert.assertEquals(actualTitle, expectedTitle);
+        driver.close();
+    }
+
+
+}
